4526366149e629da70e63b1bf2d03df0dc861c6f
coulibalyfagnine <coulibalyfagnine@gmail.com>
2020-04-07 03:23:13 +0200

    developpement de la partie front end de l'application

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = ()=>{
+    return(
+        <div className='center'>
+            <div className='navy f3'>
+                {'Hi Alas, your current rank is ...'}
+            </div>
+            <div className='navy f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
